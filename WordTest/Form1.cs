c6f652ec4e2fa5b1cfb5091e0937f1e95ce9326f
刘志辉 <879651072@qq.com>
2016-11-14 09:45:00 +0800

    add tag L08_2

--- WordTest/Form1.cs
+++ WordTest/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Windows.Forms;
 using Word=Microsoft.Office.Interop.Word;
 using Graph=Microsoft.Office.Interop.Graph;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace WordTest
 {
@@ -20,7 +21,7 @@ namespace WordTest
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Microsoft.Office.Interop.Word.Application app = new Microsoft.Office.Interop.Word.Application();
+            Word.Application app = new Word.Application();
             Word.Document doc = app.Documents.Add("D:\\Test.docx");
             app=doc.Application;
             doc.ActiveWindow.Visible = true;
@@ -28,52 +29,50 @@ namespace WordTest
             {
                 if (bk.Name == "chart")
                 {
-                    object oClassType = "MSGraph.Chart.8";
-                    Word.Range range = bk.Range;
-                    Graph.Chart wdchart = (Graph.Chart)range.InlineShapes.AddOLEObject(oClassType).OLEFormat.Object;
-                    wdchart.Application.DataSheet.Cells.Clear();//清空表格的初始数据
-                    //axis.MaximumScale = 1;//最大刻度
+                    /*方法二*/
+                    /*启动Excel并填充数据*/
+                    Excel.Application eApp = new Excel.Application();//创建Excel进程
+                    eApp.Visible = true;//设置Excel可见
+                    Excel.Workbook book = eApp.Workbooks.Add();//增加一个workboo
+                    Excel.Worksheet sheet = eApp.Worksheets[1];//获取第一个Worksheet
+                    Excel.Range range = sheet.get_Range("A1", "D2");//获取A1到D2范围内的Range
+                    //向range中插入数据
+                    range.Cells[1][1] = "姓名";
+                    range.Cells[1][2] = "成绩";
+                    range.Cells[2][1] = "张三";
+                    range.Cells[2][2] = "89";
+                    range.Cells[3][1] = "李四";
+                    range.Cells[3][2] = "100";
+                    range.Cells[4][1] = "王五";
+                    range.Cells[4][2] = "95";
+                    //插入图表
+                    Excel.Chart xlChart = book.Charts.Add();
+                    //设置图表源
+                    xlChart.SetSourceData(range);
+                    //拷贝表格
+                    Word.Range wdRange = bk.Range;
+                    range.Copy();
+                    wdRange.Paste();
+                   //拷贝图表数据到Word
+                    wdRange.SetRange(wdRange.End, wdRange.End + 1);
+                    xlChart.ChartArea.Copy();
+                    wdRange.Paste();
 
-                    //填充图表,起始的行号和列号都是1 
-                    int i, j;
-                    for (i = 0; i < 3; i++)//初始化列标头 
-                    {
-                        wdchart.Application.DataSheet.Cells[i + 1, 1] = "列" + i.ToString();
-                    }
-                    for (i = 0; i < 4; i++)//填充数据 
-                    {
-                        for (j = 0; j < 4; j++)
-                        {
-                            wdchart.Application.DataSheet.Cells[i + 2, j + 1] = i * j;
-                        }
-                    }
-
-                    //根据Y轴来画图表 
-                    wdchart.Application.PlotBy = Graph.XlRowCol.xlColumns;
-
-                    wdchart.Legend.Delete();
-                    wdchart.Height = 280;
-                    wdchart.Width = 600;
-                    //oShape.Height   =   oWord.InchesToPoints(3.57f);
-
-                    //在图片之后添加文字
-                    //range.InsertParagraphAfter();
-                    //range.InsertAfter("sars");
-
-                   /* Graph.Series ss = wdchart.SeriesCollection(1) as Graph.Series;
-                    ss.Border.Color = 8388608;
-                    ss.MarkerBackgroundColor = 8388608;
-                    ss.MarkerForegroundColor = 8388608;*/
-
-                    //更新图表并保存退出 
-                    wdchart.Application.Update();
-                    //wdchart.Application.Quit();
-                   // wdchart = null;
+                    /*方法三 只适用于office 2010及其以上版本*/
+                   /* Word.Selection sel = app.Selection;
+                    Word.InlineShape shape = sel.InlineShapes.AddChart();//插入图表
+                    Word.Chart wdChart = shape.Chart;//获取图表
+                    Word.ChartData chartData = wdChart.ChartData;//获取图表的数据
+                    Excel.Workbook dataWorkbook = (Excel.Workbook)chartData.Workbook;//获取数据对应的workbook
+                    dataWorkbook.Application.Visible = false;
+                    Excel.Worksheet dataSheet = (Excel.Worksheet)dataWorkbook.Worksheets[1]; //获取图表对应的sheet
+                    */
+                    
                 }
            }
             
-            //doc.SaveAs("E:\\Test.docx");
-            //app.Quit();
+            doc.SaveAs("E:\\Test.docx");
+            app.Quit();
         }
 
     }
