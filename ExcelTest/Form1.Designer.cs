72ca03540430e41c0f7547b1859e16d909945eae
刘志辉 <879651072@qq.com>
2016-11-24 09:45:04 +0800

    C#中操作Excel（6）—— 操作Excel的两种技术（二）--OLEDB技术

--- ExcelTest/Form1.Designer.cs
+++ ExcelTest/Form1.Designer.cs
@@ -29,23 +29,35 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(124, 123);
+            this.button1.Location = new System.Drawing.Point(31, 113);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "测试";
+            this.button1.Text = "测试COM";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(134, 113);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "测试OLEDB";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +68,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- ExcelTest/Form1.cs
+++ ExcelTest/Form1.cs
@@ -1,8 +1,8 @@
 ﻿//////////////////////////////////////////////////////////////////////////
 //////File: Form1.cs
 //////Author: Hyman
-//////Date: 2016/11/18
-//////Description: 《 C#中操作Excel（4）—— 向Excel中插入图表的两种方式》源代码
+//////Date: 2016/11/23
+//////Description: C#中操作Excel（5）—— 获取Excel中数据的两种方法
 //////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
 
@@ -16,6 +16,8 @@ using System.Text;
 using System.Windows.Forms;
 using Microsoft.Office.Interop.Excel;
 using Microsoft.Office.Core;
+using System.Reflection;
+using System.Data.OleDb;
 
 namespace ExcelTest
 {
@@ -28,59 +30,103 @@ namespace ExcelTest
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+           
             Microsoft.Office.Interop.Excel._Application app = new Microsoft.Office.Interop.Excel.Application();
             app.Visible = true;
- 
             /* 以模板方式打开Excel模板*/
             Workbook book = app.Workbooks.Add("D:\\Test.xlsx");
             /* 获取第一个sheet*/
             Worksheet sheet = book.Worksheets[1];
-            /*获取Range对象*/
-            Range range = sheet.get_Range("B2", "C4");
-            /*准备数据源*/
-            System.Data.DataTable dt = new System.Data.DataTable();
-            dt.Columns.Add("Name");
-            dt.Columns.Add("Marks");
-            DataRow row1 = dt.NewRow();
-            row1["Name"] = "Tom";
-            row1["Marks"] = "96";
-            dt.Rows.Add(row1);
-            DataRow row2 = dt.NewRow();
-            row2["Name"] = "Jerry";
-            row2["Marks"] = "91";
-            dt.Rows.Add(row2);
-            DataRow row3 = dt.NewRow();
-            row3["Name"] = "Pooly";
-            row3["Marks"] = "100";
-            dt.Rows.Add(row3);
+            
+            Range range = sheet.get_Range("A1");
+            range = range.get_Resize(4, 2);
+           
+            /*获取Excel中的数据*/
+            object _optionalValue = Missing.Value;
+            object[,] objects = new object[4, 2];
+            objects = range.get_Value(_optionalValue);
+
             /*插入数据*/
-            for (int i = 0; i < dt.Rows.Count;i++ )
-            {
-                for (int j = 0; j < dt.Columns.Count;j++ )
-                {
-                    
-                    range[j+1][i+1] = dt.Rows[i][j];
-                }
-            }
-            /*插入和sheet同级别的chart*/
-            Chart chart = book.Charts.Add();
-            /*设置表格的数据源*/
-            chart.SetSourceData(range);
-
-            /*插入sheet的子图表*/
-            Microsoft.Office.Interop.Excel.Shape shape = sheet.Shapes.AddChart();
-            /*设置表格的数据源*/
-            chart = shape.Chart;
-            chart.SetSourceData(range);
-
-         
-
+            Range rangeToInsert = sheet.get_Range("D1","E4");
+            rangeToInsert.set_Value(_optionalValue,objects);
            
             /*保存*/
             //book.SaveAs("E:\\Test.xlsx ");
             /*退出*/
             //app.Quit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            /*定义连接字符串*/
+            bool hasTitle = false;
+            string path="D:\\Test.xlsx";
+            string fileType = System.IO.Path.GetExtension(path);
+            string strCon = string.Empty;
+            if (fileType == ".xls")
+            {
+                strCon = string.Format("Provider=Microsoft.Jet.OLEDB.{0}.0;" +
+                            "Extended Properties=\"Excel {1}.0;HDR={2};IMEX=1;\";" +
+                            "Data Source={3};",
+                            (fileType == ".xls" ? 4 : 12), (fileType == ".xls" ? 8 : 12), (hasTitle ? "Yes" : "NO"), path);
+            }
+            else
+            {
+                strCon = string.Format("Provider=Microsoft.ACE.OLEDB.{0}.0;" +
+                            "Extended Properties=\"Excel {1}.0;HDR={2};IMEX=1;\";" +
+                            "Data Source={3};",
+                            (fileType == ".xls" ? 4 : 12), (fileType == ".xls" ? 8 : 12), (hasTitle ? "Yes" : "NO"), path);
+            }
+
+            /*使用SQL语句读取数据*/
+            string sheetName = "sheet1";
+            string strCom = " SELECT * FROM [" + sheetName + "$]";
+
+            /*建立数据库连接*/
+            OleDbConnection myConn = new OleDbConnection(strCon);
+
+            /*建立sql语句执行器*/
+            OleDbDataAdapter myCommand = new OleDbDataAdapter(strCom, myConn);
+
+            /*执行并读取数据*/
+            System.Data.DataTable dt = new System.Data.DataTable();
+            myConn.Open();//打开连接
+            myCommand.Fill(dt);//填充数据
+            myConn.Close();//关闭连接
+
+            /*将数据集里的数据写入到Excel*/
+            //1.创建连接字符串
+
+            String sConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;" +
+
+     "Data Source=D:/Test2.xls;" +
+
+     "Extended Properties=Excel 8.0;";
+
+            OleDbConnection cn = new OleDbConnection(sConnectionString);
+
+            string sqlCreate = "CREATE TABLE TestSheet ([姓名] VarChar,[成绩] INTEGER)";
+
+            OleDbCommand cmd = new OleDbCommand(sqlCreate, cn);
+
+            //创建Excel文件：D:/Test2.xls
+
+            cn.Open();
+
+            //创建TestSheet工作表
+
+            cmd.ExecuteNonQuery();
+
+            //添加数据
+            for (int i = 1; i < dt.Rows.Count;i++ )
+            {
+                DataRow row = dt.Rows[i];
+                cmd.CommandText = "INSERT INTO TestSheet VALUES('" + row["F1"] + "'," + row["F2"] + ")";
+                cmd.ExecuteNonQuery();
+            }
+
+            //关闭连接
+            cn.Close();
         }
 
     }
